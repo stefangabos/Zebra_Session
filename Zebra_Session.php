ad033ee6b1e8f68dc01e2bc921b2bafafdd06985
Stefan Gabos <contact@stefangabos.ro>
2016-02-19 12:54:26 +0200

    Updated version number to 2.1.4

--- Zebra_Session.php
+++ Zebra_Session.php
@@ -28,7 +28,7 @@
  *  For more resources visit {@link http://stefangabos.ro/}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    2.1.2 (last revision: February 19, 2016)
+ *  @version    2.1.4 (last revision: February 19, 2016)
  *  @copyright  (c) 2006 - 2016 Stefan Gabos
  *  @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Session
