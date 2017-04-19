0a80245d1512b9c2c407a687513cdb2e55ded79e
Stefan Gabos <contact@stefangabos.ro>
2017-04-19 11:49:44 +0300

    Additional fix for #13

--- Zebra_Session.php
+++ Zebra_Session.php
@@ -28,7 +28,7 @@
  *  For more resources visit {@link http://stefangabos.ro/}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    2.1.5 (last revision: April 13, 2017)
+ *  @version    2.1.5 (last revision: April 19, 2017)
  *  @copyright  (c) 2006 - 2017 Stefan Gabos
  *  @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Session
@@ -635,10 +635,7 @@ class Zebra_Session
      */
     public function stop() {
 
-        $this->regenerate_id();
-
         session_unset();
-
         session_destroy();
 
     }
