45380ea1de10c53040cb12c685f19c2631f50018
Stefan Gabos <contact@stefangabos.ro>
2026-07-28 12:08:27 +0300

    Bumped up version number

--- Zebra_Session.php
+++ Zebra_Session.php
@@ -9,7 +9,7 @@
  *  Read more {@link https://github.com/stefangabos/Zebra_Session/#zebra-session- here}.
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    4.2.1 (last revision: July 26, 2026)
+ *  @version    4.2.2 (last revision: July 28, 2026)
  *  @copyright  © 2006 - 2026 Stefan Gabos
  *  @license    https://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Session
